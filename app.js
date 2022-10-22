c7dce1c7f34269bd45e80bfc09c965080bdf95b2
Léo2.Martin <91684674+LeopMartin@users.noreply.github.com>
2022-10-22 23:01:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
-let tab = [];
+// let tab = [];
 
-const sendData = async () => {
-  try {
-    let i = 0;
-    let elementList = document.querySelectorAll("input, textarea");
-    elementList.forEach((input) => {
-      tab[i] = input.value;
-      i += 1;
-    });
-    const response = await fetch(
-      `http://82.165.242.38:5000/test?Email=${tab[0]}&Object=${tab[1]}&Subject=${tab[2]}`,
-      {
-        mode: "no-cors",
-        method: "POST",
-        headers: {
-          Accept: "*/*",
-          Authorization: "Basic cHJlc3RhdGFpcmVAYm5ic2l0dGVyLmNvbToxMjM0NTY=",
-        },
-      }
-    );
-  } catch (error) {
-    console.error(error);
-  }
+// const sendData = async () => {
+//   try {
+//     let i = 0;
+//     let elementList = document.querySelectorAll("input, textarea");
+//     elementList.forEach((input) => {
+//       tab[i] = input.value;
+//       i += 1;
+//     });
+//     let data = JSON.stringify(mailData)
+//     const response = await fetch(
+//       `http://127.0.0.1:5000/test?${data}`,
+//       {
+//         method: "POST",
+//         body: 'Hello world',
+
+//       }
+//     );
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+let mailData = {
+  from: "info.freelance@leop.me",
+  to: "info.freelance@leop.me",
+  subject: "test",
+  text: "test email"
 };
+
+const sendData = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://127.0.0.1:5000/');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = function() {
+    console.log(xhr.responseText);
+    if (xhr.responseText == 'success') {
+      alert('Email envoyé');
+    }
+  } 
+  
+  xhr.send(JSON.stringify(mailData));
+}
 
 let btn = document.getElementById("btn-form");
 btn.addEventListener("click", sendData);
