a8aad108116a235ef8cc072fc4df48f27edb51fc
Léo2.Martin <91684674+LeopMartin@users.noreply.github.com>
2023-06-30 16:44:46 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,4 +1,5 @@
 <?php
+
 // Set the recipient email address
 $to = 'leop.freelance@gmail.com';
 
@@ -10,15 +11,44 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-// ... Validation des données du formulaire ...
-
-// Build the email message
-$message_body = "Name: $name\n\nEmail: $email\n\nMessage:\n$message";
-
-// Send the email
-if (mail($to, $subject, $message_body)) {
-    echo 'Merci pour votre message. Nous vous contacterons bientôt.';
-} else {
-    echo 'Une erreur s\'est produite lors de l\'envoi de votre message. Veuillez réessayer plus tard.';
+// Validate the form data
+$errors = [];
+if (empty($name)) {
+    $errors[] = 'Name is required';
 }
-?>
+if (empty($email)) {
+    $errors[] = 'Email is required';
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors[] = 'Email is not valid';
+}
+if (empty($message)) {
+    $errors[] = 'Message is required';
+} elseif (strlen($message) < 10) {
+    $errors[] = 'Message must be at least 10 characters';
+}
+
+// If there are errors, display them to the user
+if (!empty($errors)) {
+    echo '<ul>';
+    foreach ($errors as $error) {
+        echo '<li>' . $error . '</li>';
+    }
+    echo '</ul>';
+} else {
+    // Set the email headers
+    $headers = array(
+        'From' => $email,
+        'Reply-To' => $email,
+        'X-Mailer' => 'PHP/' . phpversion()
+    );
+
+    // Build the email message
+    $message_body = "Name: $name\n\nEmail: $email\n\nMessage:\n$message";
+
+    // Send the email
+    if (mail($to, $subject, $message_body, $headers)) {
+        echo 'Merci pour votre message. Nous vous contacterons bientôt.';
+    } else {
+        echo 'Une erreur s\'est produite lors de l\'envoi de votre message. Veuillez réessayer plus tard.';
+    }
+}
